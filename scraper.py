8b08f5ce71f0f042b6f39fbaf449a0d00449ac1a
rdingers24 <rdinge61@lakers.mercyhurst.edu>
2026-03-25 14:36:08 +0000

    CMT2

--- scraper.py
+++ scraper.py
@@ -2,7 +2,15 @@ import pandas as pd
 
 url = "https://liquipedia.net/counterstrike/Majors"
 
+# Read all tables
 tables = pd.read_html(url)
 
-print(len(tables))
-print(tables[2])  # main majors table
+# Select the correct table (your majors table)
+majors_df = tables[2]
+
+# Save to CSV
+majors_df.to_csv("gpt_majors.csv", index=False)
+
+# Optional: print to confirm
+print(majors_df)
+print("CSV file saved as gpt_majors.csv")
